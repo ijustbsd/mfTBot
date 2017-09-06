b44358a79b36d3ff68152b203dcbfb6a048ce7f6
Sergey Baboshin <ijustbsd@gmail.com>
2017-09-06 19:15:16 +0300

    Removed duplicate initialization

--- inline_btns.py
+++ inline_btns.py
@@ -1,4 +1,5 @@
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+
 course_btns = InlineKeyboardMarkup(inline_keyboard=[[
     InlineKeyboardButton(text='1', callback_data='1'),
     InlineKeyboardButton(text='2', callback_data='2'),
@@ -7,29 +8,7 @@ course_btns = InlineKeyboardMarkup(inline_keyboard=[[
     InlineKeyboardButton(text='5', callback_data='5')
 ]])
 
-first_btns = InlineKeyboardMarkup(inline_keyboard=[
-    [
-        InlineKeyboardButton(text='1.1', callback_data='11'),
-        InlineKeyboardButton(text='1.2', callback_data='12')
-    ],
-    [
-        InlineKeyboardButton(text='2.1', callback_data='21')
-    ],
-    [
-        InlineKeyboardButton(text='3.1', callback_data='31'),
-        InlineKeyboardButton(text='3.2', callback_data='32'),
-        InlineKeyboardButton(text='3.3', callback_data='33')
-    ],
-    [
-        InlineKeyboardButton(text='4.1', callback_data='41')
-    ],
-    [
-        InlineKeyboardButton(text='5.1', callback_data='51'),
-        InlineKeyboardButton(text='5.2', callback_data='52')
-    ]
-])
-
-second_btns = InlineKeyboardMarkup(inline_keyboard=[
+first_btns = second_btns = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(text='1.1', callback_data='11'),
         InlineKeyboardButton(text='1.2', callback_data='12')

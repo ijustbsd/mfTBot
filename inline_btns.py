59ccf0e8a504783b400bde84c839c0c04ea7e701
Sergey Baboshin <ijustbsd@gmail.com>
2017-09-07 21:13:16 +0300

    Fixed bug with 4.2 inline button

--- inline_btns.py
+++ inline_btns.py
@@ -8,7 +8,7 @@ course_btns = InlineKeyboardMarkup(inline_keyboard=[[
     InlineKeyboardButton(text='5', callback_data='5')
 ]])
 
-first_btns = second_btns = InlineKeyboardMarkup(inline_keyboard=[
+first_btns = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(text='1.1', callback_data='11'),
         InlineKeyboardButton(text='1.2', callback_data='12')
@@ -23,6 +23,29 @@ first_btns = second_btns = InlineKeyboardMarkup(inline_keyboard=[
     ],
     [
         InlineKeyboardButton(text='4.1', callback_data='41')
+    ],
+    [
+        InlineKeyboardButton(text='5.1', callback_data='51'),
+        InlineKeyboardButton(text='5.2', callback_data='52')
+    ]
+])
+
+second_btns = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='1.1', callback_data='11'),
+        InlineKeyboardButton(text='1.2', callback_data='12')
+    ],
+    [
+        InlineKeyboardButton(text='2.1', callback_data='21')
+    ],
+    [
+        InlineKeyboardButton(text='3.1', callback_data='31'),
+        InlineKeyboardButton(text='3.2', callback_data='32'),
+        InlineKeyboardButton(text='3.3', callback_data='33')
+    ],
+    [
+        InlineKeyboardButton(text='4.1', callback_data='41'),
+        InlineKeyboardButton(text='4.2', callback_data='42')
     ],
     [
         InlineKeyboardButton(text='5.1', callback_data='51'),

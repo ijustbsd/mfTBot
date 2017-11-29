68d7dd8a9598501d951be1b3c7d3900403149c18
Sergey Baboshin <ijustbsd@gmail.com>
2017-11-29 17:53:23 +0300

    Updated config file

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,8 @@
-# Before using, remove "sample_" from the file name!
+# -*- coding: utf-8 -*-
+'''
+Bot config file. Before using, remove "sample_" from the file name!
+'''
 from pathlib import Path
 
 TOKEN = "PASTE YOUR TOKEN HERE"
-admins = ["chatid's"]
 PATH = str(Path(__file__).parents[0])
